9283c3afb74a4b3486aa70f7175e5e5f6c60c0f2
Andre Bossard <anbossar@microsoft.com>
2021-12-06 12:29:49 +0100

    example app

--- src/gateway.py
+++ src/gateway.py
@@ -1,13 +1,30 @@
 from flask import Flask, request
 import requests
 import os
+from azure.storage.queue import (
+        QueueClient,
+        BinaryBase64EncodePolicy,
+        BinaryBase64DecodePolicy
+)
 
 app = Flask(__name__)
-nameService = os.environ.get('NAME_SERVICE') or "http://localhost:8081"
+name_service_url = os.environ.get('NAME_SERVICE') or "http://localhost:8081"
+storage_queue_connection = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
+storate_queue_name = os.environ.get("STORAGE_QUEUE_NAME")
+queue_client = QueueClient.from_connection_string(storage_queue_connection, queue_name=storate_queue_name)
+
 
 def call_name_service(name):
-    return requests.get(nameService+"/"+name).text
+    queue_client.send_message(name)
+    return requests.get(name_service_url+"/"+name).text
+
+def trigger_message_processing():
+    return requests.get(name_service_url+"/process").text
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! You are " + call_name_service(request.user_agent.browser)
+    return "Hello, World! You are {}\n(Output: {})".format(
+        call_name_service(request.user_agent.browser), 
+        trigger_message_processing()
+        )
+
--- src/name_service.py
+++ src/name_service.py
@@ -1,12 +1,36 @@
+import os
 from flask import Flask
+from azure.storage.queue import (
+        QueueClient,
+        BinaryBase64EncodePolicy,
+        BinaryBase64DecodePolicy
+)
 
 app = Flask(__name__)
 
+storage_queue_connection = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
+storate_queue_name = os.environ.get("STORAGE_QUEUE_NAME")
+
+queue_client = QueueClient.from_connection_string(storage_queue_connection, queue_name=storate_queue_name)
+
+try:
+    queue_client.create_queue()
+except:
+    pass
+
 @app.route("/")
 def hello_world():
-    return "Please call with /name"
+    return "Please call with /name or /process to process messages"
 
 # get name from url path
 @app.route("/<name>")
 def hello_name(name):
     return "!{}!".format(name)
+
+@app.route("/process")
+def process_queue():
+    messages = queue_client.receive_messages()
+    contents = [message.content for message in messages]
+    for message in messages:
+        queue_client.delete_message(message.id, message.pop_receipt)
+    return "Processed {} messages. (Content: {})".format(len(contents), contents)
