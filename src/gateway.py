729abbec672046da1c85fe8f57d4b73d85c813bc
Andre Bossard <anbossar@microsoft.com>
2021-12-06 12:36:32 +0100

    update and cleanup

--- src/gateway.py
+++ src/gateway.py
@@ -1,30 +1,30 @@
-from flask import Flask, request
-import requests
 import os
+
+import requests
 from azure.storage.queue import (
-        QueueClient,
-        BinaryBase64EncodePolicy,
-        BinaryBase64DecodePolicy
+    QueueClient
 )
+from flask import Flask, request
 
 app = Flask(__name__)
 name_service_url = os.environ.get('NAME_SERVICE') or "http://localhost:8081"
 storage_queue_connection = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
-storate_queue_name = os.environ.get("STORAGE_QUEUE_NAME")
-queue_client = QueueClient.from_connection_string(storage_queue_connection, queue_name=storate_queue_name)
+storage_queue_name = os.environ.get("STORAGE_QUEUE_NAME")
+queue_client = QueueClient.from_connection_string(storage_queue_connection, queue_name=storage_queue_name)
 
 
 def call_name_service(name):
     queue_client.send_message(name)
-    return requests.get(name_service_url+"/"+name).text
+    return requests.get(name_service_url + "/" + name).text
+
 
 def trigger_message_processing():
-    return requests.get(name_service_url+"/process").text
+    return requests.get(name_service_url + "/process").text
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World! You are {}\n(Output: {})".format(
-        call_name_service(request.user_agent.browser), 
+        call_name_service(request.user_agent.browser),
         trigger_message_processing()
-        )
-
+    )
